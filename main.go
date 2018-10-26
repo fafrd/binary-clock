9d76e66e14ad2d8c4e47592452561ea301e1b7d2
kian <kbradley@ucmerced.edu>
2018-10-26 15:57:20 -0400

    └ box characters ┘

--- main.go
+++ main.go
@@ -27,9 +27,11 @@ func main() {
 	wg.Wait()
 }
 
-func printBCD(printme int, xoffset int) {
+func printBCD(printme int, xoffset int, drawHourCorner bool) {
 	yoffset := 1
 	fillRune := 'x'
+	fg := termbox.ColorWhite
+	bg := termbox.ColorBlack
 	/* representing the number 59 in binary-encoded-decimal:
 	8    x
 	4  x  
@@ -42,48 +44,35 @@ func printBCD(printme int, xoffset int) {
 	ones := printme % 10
 	tens := (printme - ones) / 10
 
+	// draw box
+	drawBox(drawHourCorner, xoffset, yoffset, fg, bg)
+
+	// draw tens digit
 	if tens > 7 {
-		termbox.SetCell(xoffset, yoffset, fillRune, termbox.ColorWhite, termbox.ColorBlack)
-	} else {
-		termbox.SetCell(xoffset, yoffset, ' ', termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(xoffset + 1, yoffset + 1, fillRune, termbox.ColorWhite, termbox.ColorBlack)
 	}
 	if tens > 3 && tens < 8 {
-		termbox.SetCell(xoffset, yoffset + 1, fillRune, termbox.ColorWhite, termbox.ColorBlack)
-	} else {
-		termbox.SetCell(xoffset, yoffset + 1, ' ', termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(xoffset + 1, yoffset + 3, fillRune, termbox.ColorWhite, termbox.ColorBlack)
 	}
 	if tens == 2 || tens == 3 || tens == 6 || tens == 7 {
-		termbox.SetCell(xoffset, yoffset + 2, fillRune, termbox.ColorWhite, termbox.ColorBlack)
-	} else {
-		termbox.SetCell(xoffset, yoffset + 2, ' ', termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(xoffset + 1, yoffset + 5, fillRune, termbox.ColorWhite, termbox.ColorBlack)
 	}
 	if tens % 2 == 1 {
-		termbox.SetCell(xoffset, yoffset + 3, fillRune, termbox.ColorWhite, termbox.ColorBlack)
-	} else {
-		termbox.SetCell(xoffset, yoffset + 3, ' ', termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(xoffset + 1, yoffset + 7, fillRune, termbox.ColorWhite, termbox.ColorBlack)
 	}
 
-	xoffset += 2
-
+	// draw ones digit
 	if ones > 7 {
-		termbox.SetCell(xoffset, yoffset, fillRune, termbox.ColorWhite, termbox.ColorBlack)
-	} else {
-		termbox.SetCell(xoffset, yoffset, ' ', termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(xoffset + 3, yoffset + 1, fillRune, termbox.ColorWhite, termbox.ColorBlack)
 	}
 	if ones > 3 && ones < 8 {
-		termbox.SetCell(xoffset, yoffset + 1, fillRune, termbox.ColorWhite, termbox.ColorBlack)
-	} else {
-		termbox.SetCell(xoffset, yoffset + 1, ' ', termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(xoffset + 3, yoffset + 3, fillRune, termbox.ColorWhite, termbox.ColorBlack)
 	}
 	if ones == 2 || ones == 3 || ones == 6 || ones == 7 {
-		termbox.SetCell(xoffset, yoffset + 2, fillRune, termbox.ColorWhite, termbox.ColorBlack)
-	} else {
-		termbox.SetCell(xoffset, yoffset + 2, ' ', termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(xoffset + 3, yoffset + 5, fillRune, termbox.ColorWhite, termbox.ColorBlack)
 	}
 	if ones % 2 == 1 {
-		termbox.SetCell(xoffset, yoffset + 3, fillRune, termbox.ColorWhite, termbox.ColorBlack)
-	} else {
-		termbox.SetCell(xoffset, yoffset + 3, ' ', termbox.ColorWhite, termbox.ColorBlack)
+		termbox.SetCell(xoffset + 3, yoffset + 7, fillRune, termbox.ColorWhite, termbox.ColorBlack)
 	}
 
 	termbox.Flush()
@@ -92,9 +81,9 @@ func printBCD(printme int, xoffset int) {
 func draw() {
 	for {
 		hour, minute, second := getTime()
-		printBCD(hour % 12, 0)
-		printBCD(minute, 6)
-		printBCD(second, 12)
+		printBCD(hour, 0, true)
+		printBCD(minute, 8, false)
+		printBCD(second, 16, false)
 	}
 }
 
@@ -119,5 +108,61 @@ func getTime() (int, int, int) {
 	second := t.Second()
 
 	return hour, minute, second
+}
+
+func drawBox(hour bool, xoffset int, yoffset int, fg termbox.Attribute, bg termbox.Attribute) {
+	if (hour) {
+		termbox.SetCell(xoffset, yoffset + 4, '┌', fg, bg)
+		termbox.SetCell(xoffset + 2, yoffset + 2, '├', fg, bg)
+	} else {
+		termbox.SetCell(xoffset, yoffset + 2, '┌', fg, bg)
+		termbox.SetCell(xoffset, yoffset + 3, '│', fg, bg)
+		termbox.SetCell(xoffset, yoffset + 4, '├', fg, bg)
+
+		termbox.SetCell(xoffset + 1, yoffset + 2, '─', fg, bg)
+		termbox.SetCell(xoffset + 1, yoffset + 3, ' ', fg, bg)
+
+		termbox.SetCell(xoffset + 2, yoffset + 2, '┼', fg, bg)
+	}
+
+	termbox.SetCell(xoffset, yoffset + 5, '│', fg, bg)
+	termbox.SetCell(xoffset, yoffset + 6, '├', fg, bg)
+	termbox.SetCell(xoffset, yoffset + 7, '│', fg, bg)
+	termbox.SetCell(xoffset, yoffset + 8, '└', fg, bg)
+
+	termbox.SetCell(xoffset + 1, yoffset + 4, '─', fg, bg)
+	termbox.SetCell(xoffset + 1, yoffset + 5, ' ', fg, bg)
+	termbox.SetCell(xoffset + 1, yoffset + 6, '─', fg, bg)
+	termbox.SetCell(xoffset + 1, yoffset + 7, ' ', fg, bg)
+	termbox.SetCell(xoffset + 1, yoffset + 8, '─', fg, bg)
+
+	termbox.SetCell(xoffset + 2, yoffset, '┌', fg, bg)
+	termbox.SetCell(xoffset + 2, yoffset + 1, '│', fg, bg)
+	termbox.SetCell(xoffset + 2, yoffset + 3, '│', fg, bg)
+	termbox.SetCell(xoffset + 2, yoffset + 4, '┼', fg, bg)
+	termbox.SetCell(xoffset + 2, yoffset + 5, '│', fg, bg)
+	termbox.SetCell(xoffset + 2, yoffset + 6, '┼', fg, bg)
+	termbox.SetCell(xoffset + 2, yoffset + 7, '│', fg, bg)
+	termbox.SetCell(xoffset + 2, yoffset + 8, '┴', fg, bg)
+
+	termbox.SetCell(xoffset + 3, yoffset, '─', fg, bg)
+	termbox.SetCell(xoffset + 3, yoffset + 1, ' ', fg, bg)
+	termbox.SetCell(xoffset + 3, yoffset + 2, '─', fg, bg)
+	termbox.SetCell(xoffset + 3, yoffset + 3, ' ', fg, bg)
+	termbox.SetCell(xoffset + 3, yoffset + 4, '─', fg, bg)
+	termbox.SetCell(xoffset + 3, yoffset + 5, ' ', fg, bg)
+	termbox.SetCell(xoffset + 3, yoffset + 6, '─', fg, bg)
+	termbox.SetCell(xoffset + 3, yoffset + 7, ' ', fg, bg)
+	termbox.SetCell(xoffset + 3, yoffset + 8, '─', fg, bg)
+
+	termbox.SetCell(xoffset + 4, yoffset, '┐', fg, bg)
+	termbox.SetCell(xoffset + 4, yoffset + 1, '│', fg, bg)
+	termbox.SetCell(xoffset + 4, yoffset + 2, '┤', fg, bg)
+	termbox.SetCell(xoffset + 4, yoffset + 3, '│', fg, bg)
+	termbox.SetCell(xoffset + 4, yoffset + 4, '┤', fg, bg)
+	termbox.SetCell(xoffset + 4, yoffset + 5, '│', fg, bg)
+	termbox.SetCell(xoffset + 4, yoffset + 6, '┤', fg, bg)
+	termbox.SetCell(xoffset + 4, yoffset + 7, '│', fg, bg)
+	termbox.SetCell(xoffset + 4, yoffset + 8, '┘', fg, bg)
 }
 
